bff349ad465dd427793ca3500c506ad961358cf8
Riyabaisoya05 <riyabaisoya2623@gmail.com>
2026-09-16 09:47:41 +0530

    experiment01

---
+++ experiment01/Eventemitter.js
@@ -0,0 +1,19 @@
+// node js
+const EventEmitter = require('events');// eventEmitter->class
+const myEmitter = new EventEmitter();//myEmitter->object
+myEmitter.on('greet', (name) => {//on->listener
+    console.log(`Hello, ${name}! Welcome to ABES Engineering College`);
+});
+myEmitter.on('exit',()=>{
+    console.log("Application Closed.");
+});
+myEmitter.emit('greet','2nd Year');//emit->trigger
+myEmitter.emit('exit');
+
+//half
+class DOMElement extends EventEmitter{
+    constructor(name){
+        super();
+        this.name= name;
+    }
+}
